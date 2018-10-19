1c0fe715e6d96f4f31cdd59521270abe2e6346b8
EnesOG <sagir_enes@hotmail.com>
2018-10-19 14:20:02 +0200

    Added pagination

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -2,18 +2,42 @@ import React from 'react';
 import axios from 'axios';
 import Github from "../Github/Github";
 import './Portfolio.css'
+import Pagination from 'rc-pagination'
+import 'rc-pagination/assets/index.css';
 import Zoom from 'react-reveal'
 export default class Portfolio extends React.Component {
     state = {
-        projects: []
+        projects: [],
+        currentPage : 1 ,
+        total: 9,
+
+    };
+    OnChange = (page) =>{
+       if (page !== this.state.currentPage){
+           this.setState({currentPage : page});
+
+           axios.get(`https://api.github.com/users/EnesOG/repos?page=${page}&per_page=6`)
+               .then(res => {
+                   const projects = res.data;
+                   this.setState({projects});
+               })
+       }
     };
 
     componentDidMount() {
+        axios.get(`https://api.github.com/users/EnesOG/repos`)
+            .then(res => {
+                const projects = res.data;
+                let total = 0;
+                projects.forEach((p , i) => total++ );
+                this.setState({ total});
+            });
         axios.get(`https://api.github.com/users/EnesOG/repos?page=1&per_page=6`)
             .then(res => {
                 const projects = res.data;
                 this.setState({projects});
             })
+
     }
 
     render() {
@@ -32,6 +56,7 @@ export default class Portfolio extends React.Component {
 
                 )}
             </div>
+                <Pagination className={'paginationCenter'} pageSize={6} onChange={this.OnChange} current={this.state.current} total={this.state.total} />
                 <div className=" text-center justify-content">
                 <button className="btn btn-Custom mb-2"><a className="customAhref" href="https://github.com/EnesOG?tab=repositories">See more on github</a></button>
                 </div>
